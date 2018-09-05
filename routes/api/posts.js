2c6cc5eefacb4a55d97ecb870409c60f1627f5bb
chewdev <itsbewald@gmail.com>
2018-09-04 23:20:50 -0700

    Initial commit

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+// @route GET api/posts/test
+// @desc Tests post route
+// @access Public route
+router.get("/test", (req, res) => {
+  res.json({ msg: "some post" });
+});
+
+module.exports = router;
