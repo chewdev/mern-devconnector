7a816408ccb73e647d619cab0dfe1eac9617e607
chewdev <itsbewald@gmail.com>
2018-09-12 16:30:52 -0700

    Added environment checking for applying Redux Dev Tools

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const shouldApplyDevTools =
+  process.env.NODE_ENV === "production"
+    ? null
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    shouldApplyDevTools
   )
 );
 
